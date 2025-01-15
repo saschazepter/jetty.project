24adbe93a79b773a59c5d264ef199d18748648db
Lachlan Roberts <lachlan.p.roberts@gmail.com>
2025-01-15 15:55:08 +1100

    Improve consistency in API of FormFields maxFields and maxLength
    Signed-off-by: Lachlan Roberts <lachlan.p.roberts@gmail.com>

--- jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/FormFields.java
+++ jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/FormFields.java
@@ -115,9 +115,7 @@ public class FormFields extends ContentSourceCompletableFuture<Fields>
      */
     public static Fields getFields(Request request)
     {
-        int maxFields = getContextAttribute(request.getContext(), FormFields.MAX_FIELDS_ATTRIBUTE, FormFields.MAX_FIELDS_DEFAULT);
-        int maxLength = getContextAttribute(request.getContext(), FormFields.MAX_LENGTH_ATTRIBUTE, FormFields.MAX_LENGTH_DEFAULT);
-        return getFields(request, maxFields, maxLength);
+        return getFields(request, -1, -1);
     }
 
     /**
